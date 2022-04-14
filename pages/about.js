1b450955d90de389e86173429660ff14d92ab49e
Ashwin Khode <ashwin.khode1@gmail.com>
2022-04-14 14:15:08 +0530

    add: use Next Image component in notion page

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,7 @@ import { NotionAPI } from "notion-client";
 
 // core styles shared by all of react-notion-x (required)
 import "react-notion-x/src/styles.css";
+import Image from "next/image";
 
 const pageId = process.env.ABOUT_PAGE_ID;
 
@@ -26,6 +27,24 @@ export default function Page({ recordMap }) {
       recordMap={recordMap}
       fullPage={true}
       rootPageId={pageId}
+      components={{
+        Image: (props) => (
+          <Image
+            {...props}
+            alt="notion image"
+            loading={"eager"}
+            decoding={"sync"}
+          />
+        ),
+        nextImage: (props) => (
+          <Image
+            {...props}
+            alt="notion image"
+            loading={"eager"}
+            decoding={"sync"}
+          />
+        ),
+      }}
     />
   );
 }
